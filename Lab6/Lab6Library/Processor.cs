a24ba543ad311ee6b845f124485a56c20ab7880f
Dev1neR <35506504+Dev1neR@users.noreply.github.com>
2018-11-21 14:17:03 +0200

    Lab6

---
+++ Lab6/Lab6Library/Processor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6Library
+{
+    public class Processor
+    {
+        public string Family { get; private set; }
+        public string Model { get; private set; }
+        public string Socket { get; private set; }
+        public string Cores { get; private set; }
+        public bool HasMultiplier { get; private set; }
+
+        public Processor(string family, string model, string socket, string cores)
+        {
+            this.Family = family;
+            this.Model = model;
+            this.Socket = socket;
+            this.Cores = cores;
+            this.HasMultiplier = Model.IndexOf('k') != -1 ? true : false;
+        }
+        private int freq;
+        public int Freq
+        {
+            get { return freq; }
+            set
+            {
+                if (value > 0 && value < 5000)
+                {
+                    freq = value;
+                }
+                else
+                    throw new InvalidFreqException();
+            }
+        }
+
+        public override string ToString()
+        {
+            string multiplier = HasMultiplier ? "Помножувач розблокований" + "\n" : "Помножувач заблоковано" + "\n";
+            return "------------------------------------------------\n" +
+            "Дані про процессор\n" +
+            "------------------------------------------------\n" +
+            "Процесор: " + Family + " " + Model + "\n" +
+            "Socket: " + Socket + "\n" +
+            "Кiлькiсть ядер: " + Cores + "\n" +
+            "Внутрiшня тактова частота: " + Freq + "MHz\n" + multiplier +
+            "Придбати: https://rozetka.com.ua/ua/search/?text=" + Model;
+        }
+    }
+}
